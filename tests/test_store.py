e7110329ef257e62968e18e58905dcf11269d2b4
Attack825 <68852184+Attack825@users.noreply.github.com>
2025-08-10 17:45:43 +0800

    feat: improve package src folder structure (#4)

--- tests/test_store.py
+++ tests/test_store.py
@@ -15,8 +15,8 @@
 import unittest
 
 import tests.test_util as test_util
-from src.main import CasibaseSDK
-from src.store import Store
+from src.casibase_python_sdk.main import CasibaseSDK
+from src.casibase_python_sdk.store import Store
 
 
 class StoreTest(unittest.TestCase):
